741659eaf2664452157f8927b2ee12b45dad7e95
Maja Francetic <francetic.maja@gmail.com>
2019-10-11 13:02:53 +0200

    Implement Glide to display the photo in the AstroAppWidget

--- app/src/main/java/com/udacity/astroapp/data/AstroAppWidget.java
+++ app/src/main/java/com/udacity/astroapp/data/AstroAppWidget.java
@@ -6,10 +6,17 @@ import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.view.View;
 import android.widget.RemoteViews;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.bumptech.glide.module.AppGlideModule;
+import com.bumptech.glide.request.target.AppWidgetTarget;
+import com.bumptech.glide.request.transition.Transition;
 import com.squareup.picasso.Picasso;
 import com.udacity.astroapp.R;
 import com.udacity.astroapp.activities.MainActivity;
@@ -20,57 +27,70 @@ import com.udacity.astroapp.fragments.PhotoFragment;
  */
 public class AstroAppWidget extends AppWidgetProvider {
 
+
     private static void updateAppWidget(Context context, AppWidgetManager
-            appWidgetManager, int appWidgetId) {
+            appWidgetManager, int appWidgetId, int [] appWidgetIds) {
 
         /* Create a new RemoteViews variable with the astro_app_widget.xml layout */
         RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.astro_app_widget);
+
+        AppWidgetTarget appWidgetTarget = new AppWidgetTarget(context, R.id.widget_image, remoteViews, appWidgetIds) {
+            @Override
+            public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                super.onResourceReady(resource, transition);
+            }
+        };
 
         /* Create a new intent that launches the MainActivity.class, as well as a pending Intent*/
         Intent intent = new Intent(context, MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
 
-            if (PhotoFragment.photo != null && PhotoFragment.photoUrl != null && PhotoFragment.videoUri == null) {
-                /* If the photo exists, load it into the widget_image remote view */
-                Uri photoUri = Uri.parse(PhotoFragment.photoUrl);
-                Picasso picasso = new Picasso.Builder(context).build();
-                if (photoUri != null) {
-                    picasso.load(photoUri)
-                            .into(remoteViews, R.id.widget_image, new int[]{appWidgetId});
+        if (PhotoFragment.photo != null && PhotoFragment.photoUrl != null && PhotoFragment.videoUri == null) {
+            /* If the photo exists, load it into the widget_image remote view */
+            Uri photoUri = Uri.parse(PhotoFragment.photoUrl);
 
-                    /* Set the text of the widget_image_title TextView to the photoTitle and make all
-                     * views visible */
-                    if (PhotoFragment.photoTitle != null) {
-                        remoteViews.setTextViewText(R.id.widget_image_title, PhotoFragment.photoTitle);
-                    }
-                    remoteViews.setViewVisibility(R.id.widget_image_title, View.VISIBLE);
-                    remoteViews.setViewVisibility(R.id.widget_image_label, View.VISIBLE);
-                    remoteViews.setViewVisibility(R.id.widget_image, View.VISIBLE);
-                } else {
-                    remoteViews.setViewVisibility(R.id.widget_image_label, View.GONE);
-                    remoteViews.setViewVisibility(R.id.widget_image_title, View.GONE);
-                    remoteViews.setViewVisibility(R.id.widget_image, View.VISIBLE);
-                    remoteViews.setImageViewResource(R.id.widget_image, R.mipmap.ic_launcher);
-                }
-            }  else {
-            /* In case there is no photo, hide the image label and title, and show only the
-             * ic_launcher logo */
+            if (photoUri != null) {
+                GlideApp.with(context.getApplicationContext())
+                        .asBitmap()
+                        .load(photoUri)
+                        .centerCrop()
+                        .into(appWidgetTarget);
+
+            /* Set the text of the widget_image_title TextView to the photoTitle and make all
+             * views visible */
+            if (PhotoFragment.photoTitle != null) {
+                remoteViews.setTextViewText(R.id.widget_image_title, PhotoFragment.photoTitle);
+            }
+            remoteViews.setViewVisibility(R.id.widget_image_title, View.VISIBLE);
+            remoteViews.setViewVisibility(R.id.widget_image_label, View.VISIBLE);
+            remoteViews.setViewVisibility(R.id.widget_image, View.VISIBLE);
+        } else {
             remoteViews.setViewVisibility(R.id.widget_image_label, View.GONE);
             remoteViews.setViewVisibility(R.id.widget_image_title, View.GONE);
             remoteViews.setViewVisibility(R.id.widget_image, View.VISIBLE);
             remoteViews.setImageViewResource(R.id.widget_image, R.mipmap.ic_launcher);
         }
-        /* Set an OnClickPendingIntent to the widget */
-        remoteViews.setOnClickPendingIntent(R.id.widget_layout, pendingIntent);
-        appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
+    }  else {
+        /* In case there is no photo, hide the image label and title, and show only the
+         * ic_launcher logo */
+        remoteViews.setViewVisibility(R.id.widget_image_label, View.GONE);
+        remoteViews.setViewVisibility(R.id.widget_image_title, View.GONE);
+        remoteViews.setViewVisibility(R.id.widget_image, View.VISIBLE);
+        remoteViews.setImageViewResource(R.id.widget_image, R.mipmap.ic_launcher);
     }
+    /* Set an OnClickPendingIntent to the widget */
+        remoteViews.setOnClickPendingIntent(R.id.widget_layout,pendingIntent);
+        appWidgetManager.updateAppWidget(appWidgetId,remoteViews);
+}
 
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         /* In case there are multiple widgets active, update all of them */
+
+
         for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
+            updateAppWidget(context, appWidgetManager, appWidgetId, appWidgetIds);
         }
     }
 
---
+++ app/src/main/java/com/udacity/astroapp/data/AstroGlideAppModule.java
@@ -0,0 +1,8 @@
+package com.udacity.astroapp.data;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public class AstroGlideAppModule extends AppGlideModule {
+}
